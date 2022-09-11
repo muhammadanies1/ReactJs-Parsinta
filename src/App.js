969953fa9b78dcae51367844c645c98389e18403
Muhammad Anies Wahdie <mohammad.anies@nexsoft.co.id>
2022-09-11 22:26:50 +0700

    Resusable component and props.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,40 @@
-import React, { Component } from 'react';
+import React from "react";
+import Card from "./components/Card/Card";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      firstname: "",
-      lastname: "",
-      result: ""
-    }
-  }
-
-  changeHandler = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  submitHandler = (e) => {
-    e.preventDefault()
-    this.setState({
-      name: `${this.state.firstname} ${this.state.lastname}`
-    });
-
-    this.state.firstname = "";
-    this.state.lastname = "";
-  }
-
-
-  render() {
-    const { name } = this.state;
-
-    return(
-      <div className="p-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <div className="card-header">Learn React JS</div>
-                <div className="card-body">
-                  <form onSubmit={this.submitHandler}>
-                    <div className="mb-4">
-                      <label htmlFor="firstname" className="form-label">Firstname</label>
-                      <input type="text" value={this.state.firstname} onChange={this.changeHandler} name="firstname" id="firstname" className="form-control"/>
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="lastname" className="form-label">Lastname</label>
-                      <input type="text" value={this.state.lastname} onChange={this.changeHandler} name="lastname" id="lastname" className="form-control"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-block">Show</button>
-                  </form>
-                </div>
-                <div className="card-footer">
-                  My name is {name ? name : ""}
-                </div>
-              </div>
-            </div>
+function App() {
+  return(
+    <div className="py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <Card content={{
+              imageUrl: "http://placekitten.com/g/300/200",
+              title: "First post",
+              published: "11 Sep, 2022",
+            }}
+            />
+          </div>
+          <div className="col-md-4">
+            <Card content={{
+              imageUrl: "http://placekitten.com/300/200",
+              title: "Second post",
+              published: "4 Aug, 2020",
+            }}
+            />
+          </div>
+          <div className="col-md-4">
+            <Card
+              content={{
+                imageUrl: "http://placekitten.com/g/300/200",
+                title: "Third post",
+                published: "09 Dec, 2019",
+              }}
+            />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
